6608a7d1f138e52e69c66bb0b784a013b9d7d435
Alioune TRIANDE <cheicktriande@gmail.com>
2025-01-08 11:25:47 +0000

    User profile with component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfilePhotoComponent } from './components/profile-photo/profile-photo.component';
+import { ProfileDescriptionComponent } from './components/profile-description/profile-description.component';
+import { SocialLinkComponent } from './components/social-link/social-link.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfilePhotoComponent, ProfileDescriptionComponent, SocialLinkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
